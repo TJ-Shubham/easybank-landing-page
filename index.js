41705ecd0caf94d5b72a233f619ad1ab67e7655a
TJ-Shubham <shubham.c8@outlook.com>
2023-08-26 18:53:47 +0530

    add dom for button

---
+++ index.js
@@ -0,0 +1,13 @@
+const hamburgerbtn = document.querySelector(".hamburger-menu");
+const navlinks = document.querySelector(".nav-links");
+
+hamburgerbtn.addEventListener("click", () => {
+  if (hamburgerbtn.classList.contains("active")) {
+    navlinks.style.display = "none";
+    hamburgerbtn.classList.remove("active");
+  } else {
+    hamburgerbtn.classList.add("active");
+    navlinks.style.display = "flex";
+  }
+});
+
